432c4cdf540e154efedb7bc404fb8a63fc2f645f
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-01 15:00:13 +0200

    Added first exercise

---
+++ 01_Day_Introduction/exercice_1.py
@@ -0,0 +1,46 @@
+# Do the following operationes using operands 3 and 4.
+
+# addition
+print("Addition:", 3 + 4)
+# substraction
+print("substraction: ", 3 - 4)
+# multiplication
+print("multiplication: ", 3 * 4)
+# modulus
+print("modulus: ", 3 % 4)
+# divsion
+print("division: ", 3 / 4)
+# exponential
+print("expontential: ", 3 ** 4)
+# floor division operator
+print("floor division operator: ", 3 // 4)
+
+# Write strings on the python interactive shell. The strings are the following
+
+# your name
+print("Ruben")
+# your familiy name
+print("Blanco")
+# your country
+print("Spain")
+# I am enjoying 30 days of python
+print("I am enjoying 30 days of python")
+
+# Check the data types of the following data:
+
+# 10
+print(type(10))
+# 9.8
+print(type(9.8))
+# 3.14
+print(type(3.14))
+# 4 - 4j
+print(type(4 - 4j))
+# ['Asabeneh', 'Python', 'Finland']
+print(type(['Asabeneh', 'Python', 'Finland']))
+# Your name
+print(type("Ruben"))
+# Your family name
+print(type("Blanco"))
+# Your country
+print(type("Spain"))
