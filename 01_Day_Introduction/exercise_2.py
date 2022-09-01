26bd40ac15b4fb6b1a24f7b37c15b0d3c2e05443
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-01 17:19:10 +0200

    Exercise 2 from day 01 created

---
+++ 01_Day_Introduction/exercise_2.py
@@ -0,0 +1,19 @@
+# Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+
+# List
+from cmath import sqrt
+
+
+print(type(['Ruben', 'Python', 29, 'Spain']))
+# Tupla
+print(type(('Ruben', 'Python', 29, 'Spain')))
+# Set
+print(type({'Ruben', 'Python', 29, 'Spain'}))
+# Dictionary
+print(type({'name': 'Ruben', 'language': 'Python', 'age': 29, 'Country': 'Spain'}))
+
+# Find an Euclidian distance between(2, 3) and (10, 8)
+
+
+eucDistance = sqrt((10-2)**2+(8-3)**2)
+print(eucDistance)
