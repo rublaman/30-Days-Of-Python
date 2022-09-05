a62ae3fc172d6e3b59d51946bef771c75aa2147a
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-05 18:32:05 +0200

    Added exercise_1.py

---
+++ 02_Day_Variables_builtin_functions/exercise_1.py
@@ -0,0 +1,29 @@
+"""
+  Inside this folder create a file named exercisy_1.py
+  Write a python comment saying 'Day 2: 30 Days of python programming'
+  Declare a first name variable and assign a value to it
+  Declare a last name variable and assign a value to it
+  Declare a full name variable and assign a value to it
+  Declare a country variable and assign a value to it
+  Declare a city variable and assign a value to it
+  Declare an age variable and assign a value to it
+  Declare a year variable and assign a value to it
+  Declare a variable is_married and assign a value to it
+  Declare a variable is_true and assign a value to it
+  Declare a variable is_light_on and assign a value to it
+  Declare multiple variable on one line
+"""
+
+# Day 2: 30 Days of python programming
+
+first_name = "Ruben"
+last_name = "Blanco"
+full_name = "Ruben Blanco"
+country = "Spain"
+city = "Leon"
+age = "29"
+year = "2022"
+is_married = False
+is_ligth = True
+
+fist_name, last_name, country, age, city, is_married = "Ruben", "Blanco", "Spain", "29", "Leon", False
