d4d1af2f671b0c22cb82ac193b8f9ce636856ae7
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-09 18:42:38 +0200

    Some exercises done in exercise_1.py

--- 04_Day_Strings/exercise_1.py
+++ 04_Day_Strings/exercise_1.py
@@ -19,24 +19,49 @@ print(company)
 # Print the length of the company string using len() method and print().
 print(len(company))
 # Change all the characters to uppercase letters using upper() method.
+print(company.upper())
 # Change all the characters to lowercase letters using lower() method.
+print(company.lower())
 # Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
 # Cut(slice) out the first word of Coding For All string.
+print(company[:6])
 # Check if Coding For All string contains a word Coding using the method index, find or other methods.
+print(company.index('Coding'))
+
+if 'Coding' in company:
+    print('True')
 # Replace the word coding in the string 'Coding For All' to Python.
+print(company.replace("Coding", "Coding For All"))
 # Change Python for Everyone to Python for All using the replace method or other methods.
-# Split the string 'Coding For All' using space as the separator (split()) .
+print(company.replace("Python", "everyone to Python for All"))
+# Split the string 'Coding For All' using space as the separator (split()).
+print("Coding For All".split())
 # "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+print("Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(','))
 # What is the character at index 0 in the string Coding For All.
+print('Coding For All'.find('Coding'))
 # What is the last index of the string Coding For All.
+print(len('Coding For All') - 1)
 # What character is at index 10 in "Coding For All" string.
+print('Coding For All'[10])
 # Create an acronym or an abbreviation for the name 'Python For Everyone'.
+print('Python For Everyone')
 # Create an acronym or an abbreviation for the name 'Coding For All'.
+print(''.join(e[0] for e in 'Coding For All'.split()))
 # Use index to determine the position of the first occurrence of C in Coding For All.
+print('Coding For All'.index('C'))
 # Use index to determine the position of the first occurrence of F in Coding For All.
+print('Coding For All'.index('F'))
 # Use rfind to determine the position of the last occurrence of l in Coding For All People.
+print('Coding For All People'.rfind('l'))
 # Use index or find to find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+sentence = 'You cannot end a sentence with because because because is a conjunction'
+print(sentence.index('because'))
 # Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+print(sentence.rindex('because'))
 # Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
 # Find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
 # Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
