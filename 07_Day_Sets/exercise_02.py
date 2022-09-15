59d546c7fd41cb7e5741b1d61c33d8fc34933b19
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-16 00:04:45 +0200

    Created exercises for day 07

---
+++ 07_Day_Sets/exercise_02.py
@@ -0,0 +1,7 @@
+# Join A and B
+# Find A intersection B
+# Is A subset of B
+# Are A and B disjoint sets
+# Join A with B and B with A
+# What is the symmetric difference between A and B
+# Delete the sets completely
---
+++ 07_Day_Sets/exercise_03.py
@@ -0,0 +1,3 @@
+# Convert the ages to a set and compare the length of the list and the set, which one is bigger?
+# Explain the difference between the following data types: string, list, tuple and set
+# I am a teacher and I love to inspire and teach people. How many unique words have been used in the sentence? Use the split methods and set to get the unique words.
