27add5abdeb3e860cc98bb9e405933027c999402
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-15 23:32:48 +0200

    Created exercises for day 06

---
+++ 06_Day_Tuples/exercise_1.py
@@ -0,0 +1,5 @@
+# Create an empty tuple
+# Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
+# Join brothers and sisters tuples and assign it to siblings
+# How many siblings do you have?
+# Modify the siblings tuple and add the name of your father and mother and assign it to family_members
