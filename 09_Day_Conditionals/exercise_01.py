b06ebb1b2bda9bc4a6895268f606c564dc5ebe18
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-18 23:36:56 +0200

    Finished exercise_01.py of day 09

--- 09_Day_Conditionals/exercise_01.py
+++ 09_Day_Conditionals/exercise_01.py
@@ -1,13 +1,27 @@
-# Get user input using input(“Enter your age: ”). If user is 18 or older, give feedback: You are old enough to drive. If below 18 give feedback to wait for the missing amount of years. Output:
-# Enter your age: 30
+# Get user input using input(“Enter your age: ”). If user is 18 or older, give feedback: You are old enough to drive. If below 18 give feedback to wait for the missing amount of years. Output:# Enter your age: 30
 # You are old enough to learn to drive.
 # Output:
 # Enter your age: 15
 # You need 3 more years to learn to drive.
+age = int(input('Enter your age: '))
+print('You are old enough to drive') if age >= 18 else print(
+    'Yo have to wait: {} year(s) to drive'.format(18 - age))
+
 # Compare the values of my_age and your_age using if … else. Who is older (me or you)? Use input(“Enter your age: ”) to get the age as input. You can use a nested condition to print 'year' for 1 year difference in age, 'years' for bigger differences, and a custom text if my_age = your_age. Output:
 # Enter your age: 30
 # You are 5 years older than me.
-# Get two numbers from the user using input prompt. If a is greater than b return a is greater than b, if a is less b return a is smaller than b, else a is equal to b. Output:
-# Enter number one: 4
-# Enter number two: 3
-# 4 is greater than 3
+your_age = int(input('Enter your age: '))
+my_age = 29
+
+if your_age == my_age:
+    print('We have the same age')
+elif your_age >= my_age:
+    if your_age - my_age == 1:
+        print('You are just one year olden than me')
+    else:
+        print('You are {} years older than me'.format(your_age - my_age))
+else:
+    if my_age - your_age == 1:
+        print('I\'m just one year olther than you')
+    else:
+        print('I\'m {} years older than you'.format(my_age - your_age))
