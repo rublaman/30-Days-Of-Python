be3d7783aa7629192634adb83ec605304a3ee197
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-19 23:43:17 +0200

    Finished exercise_01.py of day 10

--- 10_Day_Loops/exercise_01.py
+++ 10_Day_Loops/exercise_01.py
@@ -1,6 +1,22 @@
 # Iterate 0 to 10 using for loop, do the same using while loop.
 
+for number in range(0, 11):
+    print(number)
+
+count = 0
+while count < 11:
+    print(count)
+    count += 1
+
 # Iterate 10 to 0 using for loop, do the same using while loop.
+
+for number in range(10, -1, -1):
+    print(number)
+
+count = 10
+while count > -1:
+    print(count)
+    count -= 1
 
 # Write a loop that makes seven calls to print(), so we get on the output the following triangle:
 #
@@ -11,6 +27,10 @@
 ######
 #######
 
+for number in range(0, 7):
+    print("*" * number)
+
+
 # Use nested loops to create the following:
 # # # # # # # #
 # # # # # # # #
@@ -20,6 +40,9 @@
 # # # # # # # #
 # # # # # # # #
 # # # # # # # #
+
+for number in range(0, 8):
+    print("# " * 8)
 
 # Print the following pattern:
 # 0 x 0 = 0
@@ -34,8 +57,22 @@
 # 9 x 9 = 81
 # 10 x 10 = 100
 
+for number in range(0, 11):
+    print('{} x {} = {}'.format(number, number, number * number))
+
 # Iterate through the list, ['Python', 'Numpy','Pandas','Django', 'Flask'] using a for loop and print out the items.
+skills = ['Python', 'Numpy', 'Pandas', 'Django', 'Flask']
+for skill in skills:
+    print(skill)
 
 # Use for loop to iterate from 0 to 100 and print only even numbers
 
+for number in range(0, 101):
+    if (number % 2 == 0):
+        print(number)
+
 # Use for loop to iterate from 0 to 100 and print only odd numbers
+
+for number in range(0, 101):
+    if (number % 2 != 0):
+        print(number)
