8672d618b0a55b9492924d53bdd474f4f2635d4a
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-19 23:47:14 +0200

    Finished exercise_02.py of day 10

--- 10_Day_Loops/exercise_02.py
+++ 10_Day_Loops/exercise_02.py
@@ -1,5 +1,22 @@
-# Use for loop to iterate from 0 to 100 and print the sum of all numbers.
-# The sum of all numbers is 5050.
+# Use for loop to iterate from 0 to 100 and print the sum of all numbers. The sum of all numbers is 5050.
 
-# Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
-# The sum of all evens is 2550. And the sum of all odds is 2500.
+from sre_constants import OPCODES
+
+
+count = 0
+for n in range(0, 101):
+    count += n
+else:
+    print(count)
+
+# Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds. The sum of all evens is 2550. And the sum of all odds is 2500.
+
+evens = 0
+odds = 0
+for n in range(0, 101):
+    if (n % 2 == 0):
+        evens += n
+    else:
+        odds += n
+else:
+    print('Evens {} and {} odds'.format(evens, odds))
