84b4bda24a236fe17d6ea261a8cd8036d3b322bc
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-19 23:17:50 +0200

    Created exercises for day 10

---
+++ 10_Day_Loops/exercise_02.py
@@ -0,0 +1,5 @@
+# Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+# The sum of all numbers is 5050.
+
+# Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+# The sum of all evens is 2550. And the sum of all odds is 2500.
---
+++ 10_Day_Loops/exercise_03.py
@@ -0,0 +1,7 @@
+# Go to the data folder and use the countries.py file. Loop through the countries and extract all the countries containing the word land.
+# This is a fruit list, ['banana', 'orange', 'mango', 'lemon'] reverse the order using loop.
+fruit = ['banana', 'orange', 'mango', 'lemon']
+# Go to the data folder and use the countries_data.py file.
+# What are the total number of languages in the data
+# Find the ten most spoken languages from the data
+# Find the 10 most populated countries in the world
