5c45e161dc90dbe39b19cdb59907a070d1c88a54
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-20 22:35:58 +0200

    Created exercises for day 11

---
+++ 11_Day_Functions/exercise_02.py
@@ -0,0 +1,10 @@
+# Declare a function named evens_and_odds . It takes a positive integer as parameter and it counts number of evens and odds in the number.
+print(evens_and_odds(100))
+# The number of odds are 50.
+# The number of evens are 51.
+
+# Call your function factorial, it takes a whole number as a parameter and it return a factorial of the number
+
+# Call your function is_empty, it takes a parameter and it checks if it is empty or not
+
+# Write different functions which take lists. They should calculate_mean, calculate_median, calculate_mode, calculate_range, calculate_variance, calculate_std(standard deviation).
---
+++ 11_Day_Functions/exercise_03.py
@@ -0,0 +1,13 @@
+# Write a function called is_prime, which checks if a number is prime.
+
+# Write a functions which checks if all items are unique in the list.
+
+# Write a function which checks if all the items of the list are of the same data type.
+
+# Write a function which check if provided variable is a valid python variable
+
+# Go to the data folder and access the countries-data.py file.
+
+# Create a function called the most_spoken_languages in the world. It should return 10 or 20 most spoken languages in the world in descending order
+
+# Create a function called the most_populated_countries. It should return 10 or 20 most populated countries in descending order.
