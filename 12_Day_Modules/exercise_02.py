9e1a0b2ecac2796cabda961744443d41f29c5988
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-22 17:09:13 +0200

    Created exercises for day 12

---
+++ 12_Day_Modules/exercise_02.py
@@ -0,0 +1,8 @@
+# Write a function list_of_hexa_colors which returns any number of hexadecimal colors in an array(six hexadecimal numbers written after  # . Hexadecimal numeral system is made out of 16 symbols, 0-9 and first 6 letters of the alphabet, a-f. Check the task 6 for output examples).
+# Write a function list_of_rgb_colors which returns any number of RGB colors in an array.
+# Write a function generate_colors which can generate any number of hexa or rgb colors.
+generate_colors('hexa', 3)  # ['#a3e12f','#03ed55','#eb3d2b']
+generate_colors('hexa', 1)  # ['#b334ef']
+# ['rgb(5, 55, 175','rgb(50, 105, 100','rgb(15, 26, 80']
+generate_colors('rgb', 3)
+generate_colors('rgb', 1)  # ['rgb(33,79, 176)']
---
+++ 12_Day_Modules/exercise_03.py
@@ -0,0 +1,2 @@
+# Call your function shuffle_list, it takes a list as a parameter and it returns a shuffled list
+# Write a function which returns an array of seven random numbers in a range of 0-9. All the numbers must be unique.
