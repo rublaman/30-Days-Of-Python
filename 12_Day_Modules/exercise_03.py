e813832c2df121ca3f1b7dc939676da198e84b1d
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-23 18:05:25 +0200

    Finished exercise_03.py of day 12

--- 12_Day_Modules/exercise_03.py
+++ 12_Day_Modules/exercise_03.py
@@ -1,2 +1,25 @@
 # Call your function shuffle_list, it takes a list as a parameter and it returns a shuffled list
+
+import random
+
+
+def shuffle_list(params: list):
+    random.shuffle(params)
+    return params
+
+
+print(shuffle_list([1, "Hola", 3, 4, 5, 6, 7]))
+
+
 # Write a function which returns an array of seven random numbers in a range of 0-9. All the numbers must be unique.
+def random_array():
+    res = []
+    for _ in range(7):
+        random_number = random.randint(0, 9)
+        while (res.count(random_number) != 0):
+            random_number = random.randint(0, 9)
+        res.append(random_number)
+    print(res)
+
+
+random_array()
