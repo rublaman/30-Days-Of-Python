5b7faa2fbbf2d37707836e71d1208f45414a50d9
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-29 16:28:15 +0200

    Finished exercises_03.py of day 14

--- 14_Higher_Order_Functions/exercise_03.py
+++ 14_Higher_Order_Functions/exercise_03.py
@@ -1,4 +1,32 @@
+from countries_data import countries_data
+from pprint import pprint
+from collections import Counter
 # Use the countries_data.py(https: // github.com/Asabeneh/30-Days-Of-Python/blob/master/data/countries-data.py) file and follow the tasks below:
 # Sort countries by name, by capital, by population
+
+
+def sort_by_name(data):
+    return sorted(data, key=lambda d: d['name'])
+
+
+def sort_by_capital(data):
+    return sorted(data, key=lambda d: d['capital'])
+
+
+def sort_by_population(data):
+    return sorted(data, key=lambda d: d['population'], reverse=True)
+
+
 # Sort out the ten most spoken languages by location.
+
+res = [cto for country in countries_data for cto in country['languages']]
+a_counter = Counter(res)
+
 # Sort out the ten most populated countries.
+
+
+def sort_by_population_and_num(data, num):
+    return sorted(data, key=lambda d: d['population'], reverse=True)[:num]
+
+
+pprint(sort_by_population_and_num(countries_data, 10))
