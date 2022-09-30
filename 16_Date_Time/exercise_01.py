bbfe88478c5b5e5c58f6b8574ffbcc3b8c4a952e
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-30 16:11:14 +0200

    Created exercises for day 16

---
+++ 16_Date_Time/exercise_01.py
@@ -0,0 +1,9 @@
+# Get the current day, month, year, hour, minute and timestamp from datetime module
+# Format the current date using this format: "%m/%d/%Y, %H:%M:%S")
+# Today is 5 December, 2019. Change this time string to time.
+# Calculate the time difference between now and new year.
+# Calculate the time difference between 1 January 1970 and now.
+# Think, what can you use the datetime module for? Examples:
+#     Time series analysis
+#     To get a timestamp of any activities in an application
+#     Adding posts on a blog
