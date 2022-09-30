4928be8c89c46392636dd18f644d477ac3f2cce6
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-30 12:12:55 +0200

    Added information about real examples of decorators

---
+++ 14_Higher_Order_Functions/decorators_real_world.py
@@ -0,0 +1,108 @@
+'''
+TIMING FUNCTIONS
+  Let's start by creating a @timer decorator. It will measure the time a function 
+  takes to execute and print the duration to the console
+'''
+import math
+import functools
+import time
+
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()    # 1
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()      # 2
+        run_time = end_time - start_time    # 3
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+    return wrapper_timer
+
+
+@timer
+def waste_some_time(num_times):
+    for _ in range(num_times):
+        sum([i**2 for i in range(10000)])
+
+
+waste_some_time(100)
+
+'''
+DEBUGGING CODE
+  The following @debug decorator will print the arguments a function is called with 
+  as well as its return value every time the function is called.
+'''
+
+
+def debug(func):
+    """Print the function signature and return value"""
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]                      # 1
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
+        signature = ", ".join(args_repr + kwargs_repr)           # 3
+        print(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {value!r}")           # 4
+        return value
+    return wrapper_debug
+
+
+@debug
+def make_greeting(name, age=None):
+    if age is None:
+        return f"Howdy {name}!"
+    else:
+        return f"Whoa {name}! {age} already, you are growing up!"
+
+
+print(make_greeting("Richard", age=112))
+
+'''
+  This example might not seem immediately useful since the @debug decorator just repeats 
+  what you just wrote. It's more powerful when applied to small convenience functions that 
+  you don't call directly yourself.
+
+  The following example calculates an approximation to the mathematical constant e:
+'''
+
+# Apply a decorator to a standard library function
+math.factorial = debug(math.factorial)
+
+
+def approximate_e(terms=18):
+    return sum(1 / math.factorial(n) for n in range(terms))
+
+
+approximate_e(5)
+
+'''
+SLOWING DOWN CODE
+  Why would you want to slow down your Python code? Probably the most common use case is
+  that you want to rate-limit a function that continuously checks whether a resource—like 
+  a web page—has changed. The @slow_down decorator will sleep one second before it calls 
+  the decorated function
+'''
+
+
+def slow_down(func):
+    """Sleep 1 second before calling the function"""
+    @functools.wraps(func)
+    def wrapper_slow_down(*args, **kwargs):
+        time.sleep(1)
+        return func(*args, **kwargs)
+    return wrapper_slow_down
+
+
+@slow_down
+def countdown(from_number):
+    if from_number < 1:
+        print("Liftoff!")
+    else:
+        print(from_number)
+        countdown(from_number - 1)
+
+
+countdown(5)
