0ab3582b2d3a97a34df58486dad97c0cee3992b7
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-09-30 14:52:37 +0200

    Finished exercises_01.py od day 15

--- 15_Type_Errors/exercise_01.py
+++ 15_Type_Errors/exercise_01.py
@@ -1,1 +1,14 @@
-# Open you python interactive shell and try all the examples covered in this section.
+'''
+Open you python interactive shell and try all the examples covered in this section.
+
+SyntaxError
+NameError
+IndexError
+ModuleNotFoundError
+AttributeError
+KeyError
+TypeError
+ImportError
+ValueError
+ZeroDivisionError
+'''
