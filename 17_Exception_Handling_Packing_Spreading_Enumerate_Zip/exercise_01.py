f4aa8e51ec433bbda1068401f64aa5cdea9977dd
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-10-01 00:55:12 +0200

    Created exercises for day 17

---
+++ 17_Exception_Handling_Packing_Spreading_Enumerate_Zip/exercise_01.py
@@ -0,0 +1,3 @@
+# Unpack the first five countries and store them in a variable nordic_countries, store Estonia and Russia in es, and ru respectively.
+names = ['Finland', 'Sweden', 'Norway',
+         'Denmark', 'Iceland', 'Estonia', 'Russia']
