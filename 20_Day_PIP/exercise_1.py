ad091dc3c81fae1482090f3b865f347c840c7745
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-10-08 22:37:58 +0200

    Created exercise for day 20

---
+++ 20_Day_PIP/exercise_1.py
@@ -0,0 +1,14 @@
+'''
+1. Read this url and find the 10 most frequent words. romeo_and_juliet = 'http://www.gutenberg.org/files/1112/1112.txt'
+2. Read the cats API and cats_api = 'https://api.thecatapi.com/v1/breeds' and find :
+  the min, max, mean, median, standard deviation of cats' weight in metric units.
+  the min, max, mean, median, standard deviation of cats' lifespan in years.
+  Create a frequency table of country and breed of cats
+3. Read the countries API and find
+  the 10 largest countries
+  the 10 most spoken languages
+  the total number of languages in the countries API
+4. UCI is one of the most common places to get data sets for data science and machine learning. 
+Read the content of UCL (https://archive.ics.uci.edu/ml/datasets.php). Without additional libraries 
+it will be difficult, so you may try it with BeautifulSoup4
+'''
