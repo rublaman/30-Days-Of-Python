11b7a4395a08e2bb225813e615c097939f4536dc
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-10-09 23:17:21 +0200

    Added requests examples

---
+++ 20_Day_PIP/fetch_json_data.py
@@ -0,0 +1,8 @@
+import requests
+url = 'https://restcountries.eu/rest/v2/all'  # countries api
+response = requests.get(url)  # opening a network and fetching a data
+print(response)  # response object
+print(response.status_code)  # status code, success:200
+countries = response.json()
+# we sliced only the first country, remove the slicing to see all countries
+print(countries[:1])
