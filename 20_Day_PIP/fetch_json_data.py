deb76a08e1cd36813bb29e37806422757f94f39b
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-10-09 23:25:11 +0200

    Refactored fecth_json_data.py

--- 20_Day_PIP/fetch_json_data.py
+++ 20_Day_PIP/fetch_json_data.py
@@ -1,8 +1,9 @@
 import requests
+import pprint
 url = 'https://restcountries.com/v3.1/all'  # countries api
 response = requests.get(url)  # opening a network and fetching a data
 print(response)  # response object
 print(response.status_code)  # status code, success:200
 countries = response.json()
 # we sliced only the first country, remove the slicing to see all countries
-print(countries[:1])
+pprint.pprint(countries[:1])
