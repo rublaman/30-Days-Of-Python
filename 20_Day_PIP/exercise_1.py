a3ef951c795dcfb731203d0c473b4df60134687d
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-10-11 22:05:54 +0200

    Exercise 2 from exercise_1.py completed

--- 20_Day_PIP/exercise_1.py
+++ 20_Day_PIP/exercise_1.py
@@ -2,15 +2,17 @@
 1. Read this url and find the 10 most frequent words. romeo_and_juliet = 'http://www.gutenberg.org/files/1112/1112.txt'
 '''
 
+import pprint
+import json
 import requests
 from collections import Counter
 
-url = 'http://www.gutenberg.org/files/1112/1112.txt'
+# url = 'http://www.gutenberg.org/files/1112/1112.txt'
 
-response = requests.get(url)
-words = response.text.split()
-top_words = Counter(words).most_common(10)
-print(top_words)
+# response = requests.get(url)
+# words = response.text.split()
+# top_words = Counter(words).most_common(10)
+# print(top_words)
 
 
 '''
@@ -19,6 +21,23 @@ print(top_words)
     the min, max, mean, median, standard deviation of cats' lifespan in years.
     Create a frequency table of country and breed of cats
 '''
+
+url_cat = 'https://api.thecatapi.com/v1/breeds'
+response_cat = requests.get(url_cat)
+cats_res_json = response_cat.json()
+
+weights = []
+cleaned = []
+
+for cat in cats_res_json:
+    weights.append(cat['weight']['metric'])
+
+aux = list([s.replace(' - ', '') for s in weights])
+cleaned = list(int(e[1:]) - int(e[0]) for e in aux)
+
+
+print(cleaned)
+
 
 '''
 3. Read the countries API and find
