cbc934571e4d411edd38446cf02e9f6792614f0c
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-10-13 23:12:08 +0200

    Created exercises for day 21

---
+++ 21_OOP/exercises_2.py
@@ -0,0 +1,5 @@
+'''
+Create a class called PersonAccount. It has firstname, lastname, incomes, expenses properties 
+and it has total_income, total_expense, account_info, add_income, add_expense and account_balance 
+methods. Incomes is a set of incomes and its description. The same goes for expenses.
+'''
