826ca058be6c931bddf9f1d4b096c797b3157aff
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-10-17 23:34:05 +0200

    Added scraping in url

--- 22_Web_Scraping/exercise_1.py
+++ 22_Web_Scraping/exercise_1.py
@@ -1,3 +1,17 @@
 '''
 Scrape the following website and store the data as json file(url = 'http://www.bu.edu/president/boston-university-facts-stats/').
 '''
+
+import requests
+from bs4 import BeautifulSoup
+url = 'http://www.bu.edu/president/boston-university-facts-stats/'
+
+res = requests.get(url)
+content = res.content
+status = res.status_code
+print(status)
+
+soup = BeautifulSoup(content, 'html.parser')
+print(soup.title.getText())
+sections = soup.find_all('div', {'class': 'facts-wrapper'})
+print(sections)
