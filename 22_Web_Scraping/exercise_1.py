25c8a880076a3438d458240ae09e15e10c9df5e8
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-10-18 23:10:04 +0200

    Completed scraping in exercise 1

--- 22_Web_Scraping/exercise_1.py
+++ 22_Web_Scraping/exercise_1.py
@@ -4,6 +4,8 @@ Scrape the following website and store the data as json file(url = 'http://www.b
 
 import requests
 from bs4 import BeautifulSoup
+import json
+import pprint
 url = 'http://www.bu.edu/president/boston-university-facts-stats/'
 
 res = requests.get(url)
@@ -14,4 +16,16 @@ print(status)
 soup = BeautifulSoup(content, 'html.parser')
 print(soup.title.getText())
 sections = soup.find_all('div', {'class': 'facts-wrapper'})
-print(sections)
+
+list_scraping = []
+
+for section in sections:
+    header = section.find('h5').text
+    res = {'Section': header, 'Categories': {}}
+    for li in section.find('ul'):
+        categories = li.text.split("\n")
+        if categories[1] != '':
+            res['Categories'][categories[1]] = categories[2]
+    list_scraping.append(res)
+
+pprint.pprint(list_scraping)
--- 22_Web_Scraping/test_web_scraping.py
+++ 22_Web_Scraping/test_web_scraping.py
@@ -3,17 +3,16 @@ from bs4 import BeautifulSoup
 url = 'https://archive.ics.uci.edu/ml/datasets.php'
 
 response = requests.get(url)
-content = response.content  # we get all the content from the website
-# beautiful soup will give a chance to parse
+content = response.content
+
 soup = BeautifulSoup(content, 'html.parser')
-print(soup.title)  # <title>UCI Machine Learning Repository: Data Sets</title>
-print(soup.title.get_text())  # UCI Machine Learning Repository: Data Sets
-print(soup.body)  # gives the whole page on the website
+print(soup.title)
+print(soup.title.get_text())
+print(soup.body)
 print(response.status_code)
 
 tables = soup.find_all('table', {'cellpadding': '3'})
-# We are targeting the table with cellpadding attribute with the value of 3
-# We can select using id, class or HTML tag , for more information check the beautifulsoup doc
-table = tables[0]  # the result is a list, we are taking out data from it
+
+table = tables[0]
 for td in table.find('tr').find_all('td'):
     print(td.text)
