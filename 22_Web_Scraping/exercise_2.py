a770d4941b79635639385fbc9fc7e216b7f30bc8
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-10-19 23:50:00 +0200

    Added scarping in url exercise 2

--- 22_Web_Scraping/exercise_2.py
+++ 22_Web_Scraping/exercise_2.py
@@ -1,3 +1,22 @@
 '''
 Extract the table in this url (https://archive.ics.uci.edu/ml/datasets.php) and change it to a json file
 '''
+
+import requests
+from bs4 import BeautifulSoup
+import json
+import pprint
+url = 'https://archive.ics.uci.edu/ml/datasets.php'
+
+res = requests.get(url)
+content = res.content
+status = res.status_code
+print(status)
+
+soup = BeautifulSoup(content, 'html.parser')
+tables = soup.find_all('table', {'cellpadding': '3'})
+
+table = tables[0]
+
+for tr in table.find_all("tr"):
+    print(tr.text.replace(u'\xa0', u' ').split('\n'))
