ece761b3ae14ce40ca3407e0074035827e704ba9
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-10-20 22:25:58 +0200

    Added scraping in exercises 3

--- 22_Web_Scraping/exercise_3.py
+++ 22_Web_Scraping/exercise_3.py
@@ -1,3 +1,14 @@
 '''
-Scrape the presidents table and store the data as json(https: // en.wikipedia.org/wiki/List_of_presidents_of_the_United_States). The table is not very structured and the scrapping may take very long time.
+Scrape the presidents table and store the data as json(https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States). The table is not very structured and the scrapping may take very long time.
 '''
+
+import requests
+from bs4 import BeautifulSoup
+import json
+import pprint
+url = 'https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States'
+
+res = requests.get(url)
+content = res.content
+status = res.status_code
+print(status)
