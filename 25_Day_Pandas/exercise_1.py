2627ecaa347a3ce4524adb18aab31671adf1f9ee
Ruben Blanco <rubenblancomanzano@gmail.com>
2022-11-05 23:47:08 +0100

    Added exercise_1.py from day 25

---
+++ 25_Day_Pandas/exercise_1.py
@@ -0,0 +1,10 @@
+'''
+  Read the hacker_news.csv file from data directory
+  Get the first five rows
+  Get the last five rows
+  Get the title column as pandas series
+  Count the number of rows and columns
+  Filter the titles which contain python
+  Filter the titles which contain JavaScript
+  Explore the data and make sense of it
+'''
